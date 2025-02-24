d0c186290fb29928db1b44677911719568522b59
Subhash Khileri <skhileri@redhat.com>
2025-02-24 17:26:44 +0530

    add github plugins tests

--- e2e-tests/playwright/e2e/github-happy-path.spec.ts
+++ e2e-tests/playwright/e2e/github-happy-path.spec.ts
@@ -78,6 +78,10 @@ test.describe.serial("GitHub Happy path", () => {
     await common.loginAsGithubUser();
   });
 
+  test("Login as a Github user.", async () => {
+    await common.loginAsGithubUser();
+  });
+
   test("Verify Profile is Github Account Name in the Settings page", async () => {
     await uiHelper.goToSettingsPage();
     await expect(page).toHaveURL("/settings");
