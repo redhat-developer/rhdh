53da9915326858b5b27a6b1d45ae2d80d7c0f799
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-02-26 15:32:35 +0100

    Working setup on AKS

--- .ibm/pipelines/cluster/aks/aks-operator-deployment.sh
+++ .ibm/pipelines/cluster/aks/aks-operator-deployment.sh
@@ -10,7 +10,7 @@ initiate_aks_operator_deployment() {
   local rhdh_base_url="https://${K8S_CLUSTER_ROUTER_BASE}"
 
   configure_namespace "${name_space}"
-  # deploy_test_backstage_provider "${name_space}" # Doesn't work on K8S
+  # deploy_test_backstage_customization_provider "${name_space}" # Doesn't work on K8S
   apply_yaml_files "${DIR}" "${name_space}" "${rhdh_base_url}"
 
   # Create a ConfigMap for dynamic plugins
@@ -25,7 +25,7 @@ initiate_aks_operator_deployment() {
 
   deploy_rhdh_operator "${name_space}" "${DIR}/resources/rhdh-operator/rhdh-start_K8s.yaml"
 
-  operation_aks_operator_ingress create "$name_space"
+  apply_aks_operator_ingress backstage-rhdh "$name_space"
 }
 
 initiate_rbac_aks_operator_deployment() {
@@ -33,13 +33,13 @@ initiate_rbac_aks_operator_deployment() {
   local rhdh_base_url="https://${K8S_CLUSTER_ROUTER_BASE}"
 
   configure_namespace "${name_space}"
-  # deploy_test_backstage_provider "${name_space}" # Doesn't work on K8S
+  # deploy_test_backstage_customization_provider "${name_space}" # Doesn't work on K8S
   create_conditional_policies_operator /tmp/conditional-policies.yaml
   prepare_operator_app_config "${DIR}/resources/config_map/app-config-rhdh-rbac.yaml"
   apply_yaml_files "${DIR}" "${name_space}" "${rhdh_base_url}"
 
   # Create a ConfigMap for dynamic plugins
-  yq_merge_value_files "merge" "${DIR}/value_files/${HELM_CHART_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_RBAC_AKS_DIFF_VALUE_FILE_NAME}" "/tmp/${HELM_CHART_K8S_MERGED_VALUE_FILE_NAME}"
+  yq_merge_value_files "merge" "${DIR}/value_files/${HELM_CHART_RBAC_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_RBAC_AKS_DIFF_VALUE_FILE_NAME}" "/tmp/${HELM_CHART_K8S_MERGED_VALUE_FILE_NAME}"
   create_dynamic_plugins_config "/tmp/${HELM_CHART_K8S_MERGED_VALUE_FILE_NAME}" "/tmp/configmap-dynamic-plugins-rbac.yaml"
   mkdir -p "${ARTIFACT_DIR}/${name_space}"
   cp -a "/tmp/configmap-dynamic-plugins-rbac.yaml" "${ARTIFACT_DIR}/${name_space}/" # Save the final value-file into the artifacts directory.
@@ -49,17 +49,18 @@ initiate_rbac_aks_operator_deployment() {
 
   deploy_rhdh_operator "${name_space}" "${DIR}/resources/rhdh-operator/rhdh-start-rbac_K8s.yaml"
 
-  operation_aks_operator_ingress create "$name_space"
+  apply_aks_operator_ingress backstage-rhdh-rbac "$name_space"
 }
 
-operation_aks_operator_ingress() {
-  local operation=$1
+apply_aks_operator_ingress() {
+  local service_name=$1
   local name_space=$2
-  kubectl $operation -f "$DIR/cluster/aks/manifest/aks-operator-ingress.yaml" --namespace="${name_space}"
+  cat "$DIR/cluster/aks/manifest/aks-operator-ingress.yaml" | \
+    yq ".spec.rules[0].http.paths[0].backend.service.name = \"$service_name\"" - | \
+    kubectl apply --namespace="${name_space}" -f -
 }
 
 cleanup_aks_deployment() {
   local name_space=$1
-  operation_aks_operator_ingress delete "$name_space"
   delete_namespace "$name_space"
 }
