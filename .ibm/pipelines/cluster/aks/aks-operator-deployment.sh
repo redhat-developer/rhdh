2f3bbd74f4e053f87d976fb0f996adc529b6507f
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-02-26 18:30:04 +0100

    Refactor name_space

--- .ibm/pipelines/cluster/aks/aks-operator-deployment.sh
+++ .ibm/pipelines/cluster/aks/aks-operator-deployment.sh
@@ -6,66 +6,66 @@ source "$DIR"/utils.sh
 source "$DIR"/install-methods/operator.sh
 
 initiate_aks_operator_deployment() {
-  local name_space=$1
+  local namespace=$1
   local rhdh_base_url=$2
 
   echo "Initiating Operator-backed non-RBAC deployment on AKS"
 
-  configure_namespace "${name_space}"
-  # deploy_test_backstage_customization_provider "${name_space}" # Doesn't work on K8s
-  apply_yaml_files "${DIR}" "${name_space}" "${rhdh_base_url}"
+  configure_namespace "${namespace}"
+  # deploy_test_backstage_customization_provider "${namespace}" # Doesn't work on K8s
+  apply_yaml_files "${DIR}" "${namespace}" "${rhdh_base_url}"
 
   echo "Creating and applying ConfigMap for dynamic plugins"
   yq_merge_value_files "merge" "${DIR}/value_files/${HELM_CHART_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_AKS_DIFF_VALUE_FILE_NAME}" "/tmp/${HELM_CHART_K8S_MERGED_VALUE_FILE_NAME}"
   create_dynamic_plugins_config "/tmp/${HELM_CHART_K8S_MERGED_VALUE_FILE_NAME}" "/tmp/configmap-dynamic-plugins.yaml"
-  mkdir -p "${ARTIFACT_DIR}/${name_space}"
-  cp -a "/tmp/configmap-dynamic-plugins.yaml" "${ARTIFACT_DIR}/${name_space}/" # Save the final value-file into the artifacts directory.
-  kubectl apply -f /tmp/configmap-dynamic-plugins.yaml -n "${name_space}"
+  mkdir -p "${ARTIFACT_DIR}/${namespace}"
+  cp -a "/tmp/configmap-dynamic-plugins.yaml" "${ARTIFACT_DIR}/${namespace}/" # Save the final value-file into the artifacts directory.
+  kubectl apply -f /tmp/configmap-dynamic-plugins.yaml -n "${namespace}"
 
-  kubectl apply -f "$DIR/resources/redis-cache/redis-deployment.yaml" --namespace="${name_space}"
+  kubectl apply -f "$DIR/resources/redis-cache/redis-deployment.yaml" --namespace="${namespace}"
 
-  setup_image_pull_secret "${name_space}" "rh-pull-secret" "${REGISTRY_REDHAT_IO_SERVICE_ACCOUNT_DOCKERCONFIGJSON}"
+  setup_image_pull_secret "${namespace}" "rh-pull-secret" "${REGISTRY_REDHAT_IO_SERVICE_ACCOUNT_DOCKERCONFIGJSON}"
 
-  deploy_rhdh_operator "${name_space}" "${DIR}/resources/rhdh-operator/rhdh-start_K8s.yaml"
+  deploy_rhdh_operator "${namespace}" "${DIR}/resources/rhdh-operator/rhdh-start_K8s.yaml"
 
-  apply_aks_operator_ingress "backstage-$RELEASE_NAME" "$name_space"
+  apply_aks_operator_ingress "backstage-$RELEASE_NAME" "$namespace"
 }
 
 initiate_rbac_aks_operator_deployment() {
-  local name_space=$1
+  local namespace=$1
   local rhdh_base_url=$2
 
   echo "Initiating Operator-backed RBAC deployment on AKS"
 
-  configure_namespace "${name_space}"
-  # deploy_test_backstage_customization_provider "${name_space}" # Doesn't work on K8s
+  configure_namespace "${namespace}"
+  # deploy_test_backstage_customization_provider "${namespace}" # Doesn't work on K8s
   create_conditional_policies_operator /tmp/conditional-policies.yaml
   prepare_operator_app_config "${DIR}/resources/config_map/app-config-rhdh-rbac.yaml"
-  apply_yaml_files "${DIR}" "${name_space}" "${rhdh_base_url}"
+  apply_yaml_files "${DIR}" "${namespace}" "${rhdh_base_url}"
 
   echo "Creating and applying ConfigMap for dynamic plugins"
   yq_merge_value_files "merge" "${DIR}/value_files/${HELM_CHART_RBAC_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_RBAC_AKS_DIFF_VALUE_FILE_NAME}" "/tmp/${HELM_CHART_K8S_MERGED_VALUE_FILE_NAME}"
   create_dynamic_plugins_config "/tmp/${HELM_CHART_K8S_MERGED_VALUE_FILE_NAME}" "/tmp/configmap-dynamic-plugins-rbac.yaml"
-  mkdir -p "${ARTIFACT_DIR}/${name_space}"
-  cp -a "/tmp/configmap-dynamic-plugins-rbac.yaml" "${ARTIFACT_DIR}/${name_space}/" # Save the final value-file into the artifacts directory.
-  kubectl apply -f /tmp/configmap-dynamic-plugins-rbac.yaml -n "${name_space}"
+  mkdir -p "${ARTIFACT_DIR}/${namespace}"
+  cp -a "/tmp/configmap-dynamic-plugins-rbac.yaml" "${ARTIFACT_DIR}/${namespace}/" # Save the final value-file into the artifacts directory.
+  kubectl apply -f /tmp/configmap-dynamic-plugins-rbac.yaml -n "${namespace}"
 
-  setup_image_pull_secret "${name_space}" "rh-pull-secret" "${REGISTRY_REDHAT_IO_SERVICE_ACCOUNT_DOCKERCONFIGJSON}"
+  setup_image_pull_secret "${namespace}" "rh-pull-secret" "${REGISTRY_REDHAT_IO_SERVICE_ACCOUNT_DOCKERCONFIGJSON}"
 
-  deploy_rhdh_operator "${name_space}" "${DIR}/resources/rhdh-operator/rhdh-start-rbac_K8s.yaml"
+  deploy_rhdh_operator "${namespace}" "${DIR}/resources/rhdh-operator/rhdh-start-rbac_K8s.yaml"
 
-  apply_aks_operator_ingress "backstage-$RELEASE_NAME_RBAC" "$name_space"
+  apply_aks_operator_ingress "backstage-$RELEASE_NAME_RBAC" "$namespace"
 }
 
 apply_aks_operator_ingress() {
   local service_name=$1
-  local name_space=$2
+  local namespace=$2
   cat "$DIR/cluster/aks/manifest/aks-operator-ingress.yaml" | \
     yq ".spec.rules[0].http.paths[0].backend.service.name = \"$service_name\"" - | \
-    kubectl apply --namespace="${name_space}" -f -
+    kubectl apply --namespace="${namespace}" -f -
 }
 
 cleanup_aks_deployment() {
-  local name_space=$1
-  delete_namespace "$name_space"
+  local namespace=$1
+  delete_namespace "$namespace"
 }
