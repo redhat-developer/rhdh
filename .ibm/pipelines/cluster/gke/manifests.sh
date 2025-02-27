418436e7c540faf5ef10d3ccadea0ade348a0a3f
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-02-27 10:04:30 +0100

    FrontendConfig and Ingress without namespace

--- .ibm/pipelines/cluster/gke/manifests.sh
+++ .ibm/pipelines/cluster/gke/manifests.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 apply_gke_frontend_config() {
-  local namespace=$1
   echo "Applying GKE Frontend Config"
-  kubectl apply -f "${DIR}/cluster/gke/manifest/frontend-config.yaml" --namespace="${namespace}"
+  kubectl apply -f "${DIR}/cluster/gke/manifest/frontend-config.yaml" 
 }
 
 apply_gke_operator_ingress() {
-  local namespace=$1
-  local service_name=$2
+  local service_name=$1
   echo "Applying GKE Ingress"
   export SERVICE_NAME=$service_name
-  envsubst < "${DIR}/cluster/gke/manifest/gke-operator-ingress.yaml" | kubectl apply --namespace="${namespace}" -f -
+  envsubst < "${DIR}/cluster/gke/manifest/gke-operator-ingress.yaml" | kubectl apply -f -
 }
