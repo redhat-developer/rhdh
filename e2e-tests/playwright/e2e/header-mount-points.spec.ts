7cf69a3e8220fd116a4179f65f92b3d3c2c62228
Yi Cai <yicai@redhat.com>
2025-02-27 11:33:02 -0500

    updated config
    Signed-off-by: Yi Cai <yicai@redhat.com>

--- e2e-tests/playwright/e2e/header-mount-points.spec.ts
+++ e2e-tests/playwright/e2e/header-mount-points.spec.ts
@@ -1,13 +1,24 @@
 import { expect, test } from "@playwright/test";
+import { UIhelper } from "../utils/ui-helper";
 import { Common } from "../utils/common";
 
 test.describe("Header mount points", () => {
   let common: Common;
+  let uiHelper: UIhelper;
 
   test.beforeEach(async ({ page }) => {
     common = new Common(page);
+    uiHelper = new UIhelper(page);
     await common.loginAsGuest();
     await expect(page.locator("nav[id='global-header']")).toBeVisible();
+  });
+
+  test("Verify that additional logo component in global header is visible", async ({
+    page,
+  }) => {
+    const header = page.locator("nav[id='global-header']");
+    await expect(header).toBeVisible();
+    uiHelper.verifyLink({ label: "test-logo" });
   });
 
   test("Verify that additional header button component from a custom header plugin in global header is visible", async ({
