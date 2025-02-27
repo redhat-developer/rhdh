a8d9dcd7a62fbc09d6ecd0e5517f4d24edd0fec2
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-02-27 10:04:30 +0100

    FrontendConfig and Ingress without namespace

--- .ibm/pipelines/cluster/gke/gke-helm-deployment.sh
+++ .ibm/pipelines/cluster/gke/gke-helm-deployment.sh
@@ -14,7 +14,7 @@ initiate_gke_helm_deployment() {
   cd "${DIR}" || exit
   local rhdh_base_url="https://${K8S_CLUSTER_ROUTER_BASE}"
   apply_yaml_files "${DIR}" "${NAME_SPACE_K8S}" "${rhdh_base_url}"
-  apply_gke_frontend_config "${NAME_SPACE_K8S}"
+  apply_gke_frontend_config
   kubectl apply -f "$DIR/resources/redis-cache/redis-deployment.yaml" --namespace="${namespace}"
   yq_merge_value_files "merge" "${DIR}/value_files/${HELM_CHART_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_GKE_DIFF_VALUE_FILE_NAME}" "/tmp/${HELM_CHART_K8S_MERGED_VALUE_FILE_NAME}"
   mkdir -p "${ARTIFACT_DIR}/${NAME_SPACE_K8S}"
@@ -36,7 +36,7 @@ initiate_rbac_gke_helm_deployment() {
   cd "${DIR}" || exit
   local rbac_rhdh_base_url="https://${K8S_CLUSTER_ROUTER_BASE}"
   apply_yaml_files "${DIR}" "${NAME_SPACE_RBAC_K8S}"  "${rbac_rhdh_base_url}"
-  apply_gke_frontend_config "${NAME_SPACE_RBAC_K8S}"
+  apply_gke_frontend_config
   yq_merge_value_files "merge" "${DIR}/value_files/${HELM_CHART_RBAC_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_RBAC_GKE_DIFF_VALUE_FILE_NAME}" "/tmp/${HELM_CHART_RBAC_K8S_MERGED_VALUE_FILE_NAME}"
   mkdir -p "${ARTIFACT_DIR}/${NAME_SPACE_RBAC_K8S}"
   cp -a "/tmp/${HELM_CHART_RBAC_K8S_MERGED_VALUE_FILE_NAME}" "${ARTIFACT_DIR}/${NAME_SPACE_RBAC_K8S}/" # Save the final value-file into the artifacts directory.
