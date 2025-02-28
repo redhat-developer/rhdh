e2e1caede05bb9e9dff8912b898c0872e477aef9
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-02-28 21:49:48 +0100

    Make redis work

--- e2e-tests/playwright/e2e/verify-redis-cache.spec.ts
+++ e2e-tests/playwright/e2e/verify-redis-cache.spec.ts
@@ -13,13 +13,22 @@ test.describe("Verify Redis Cache DB", () => {
     common = new Common(page);
     await common.loginAsGuest();
 
+    console.log("Starting port-forward process...");
     portForward = spawn("/bin/sh", [
       "-c",
       `
-      oc login --token="${process.env.K8S_CLUSTER_TOKEN}" --server="${process.env.K8S_CLUSTER_URL}" &&
-      kubectl port-forward service/redis 6379:6379 -n ${process.env.NAME_SPACE}
+      oc login --token="${process.env.K8S_CLUSTER_TOKEN}" --server="${process.env.K8S_CLUSTER_URL}" --insecure-skip-tls-verify=true && \
+      oc port-forward service/redis 6379:6379 --namespace="${process.env.NAME_SPACE}"
     `,
     ]);
+
+    portForward.stdout.on("data", (data) => {
+      console.log(`Port-forward stdout: ${data}`);
+    });
+
+    portForward.stderr.on("data", (data) => {
+      console.error(`Port-forward stderr: ${data}`);
+    });
   });
 
   test("Open techdoc and verify the cache generated in redis db", async () => {
@@ -29,7 +38,6 @@ test.describe("Verify Redis Cache DB", () => {
     await uiHelper.openSidebar("Docs");
     await uiHelper.clickLink("Backstage Showcase");
 
-    // ensure that the docs are generated. if redis configuration has an error, this page will hang and docs won't be generated
     await expect(async () => {
       await uiHelper.verifyHeading("rhdh");
     }).toPass({
@@ -37,28 +45,32 @@ test.describe("Verify Redis Cache DB", () => {
       timeout: 60_000,
     });
 
-    // Wait for port-forward to be ready
+    console.log("Waiting for port-forward to be ready...");
     await new Promise<void>((resolve, reject) => {
       portForward.stdout.on("data", (data) => {
         if (data.toString().includes("Forwarding from 127.0.0.1:6379")) {
+          console.log("Port-forward is ready.");
           resolve();
         }
       });
 
       portForward.stderr.on("data", (data) => {
+        console.error(`Port forwarding failed: ${data.toString()}`);
         reject(new Error(`Port forwarding failed: ${data.toString()}`));
       });
     });
 
-    const redis = new Redis(
-      `redis://${process.env.REDIS_TEMP_USER}:${process.env.REDIS_TEMP_PASS}@localhost:6379`,
-    );
+    console.log("Connecting to Redis...");
+    const redis = new Redis(`redis://temp:test123@localhost:6379`);
+
+    console.log("Verifying Redis keys...");
     await expect(async () => {
       const keys = await redis.keys("*");
+      console.log(`Redis keys: ${keys}`);
       expect(keys).toContainEqual(expect.stringContaining("techdocs"));
 
-      // Additionally, verify the format of the key
       const key = keys.filter((k) => k.startsWith("techdocs"))[0];
+      console.log(`Verifying key format: ${key}`);
       expect(key).toMatch(
         /(?:techdocs|bulk-import):(?:[A-Za-z0-9+]{4})*(?:[A-Za-z0-9+]{2}==|[A-Za-z0-9+]{3}=)$/gm,
       );
@@ -69,6 +81,7 @@ test.describe("Verify Redis Cache DB", () => {
   });
 
   test.afterAll(() => {
+    console.log("Killing port-forward process...");
     portForward.kill("SIGKILL");
   });
 });
