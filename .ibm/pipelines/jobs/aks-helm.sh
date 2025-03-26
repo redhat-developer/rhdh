af6c75d99500d2086153eff559b72c779505336c
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-03-26 11:28:28 +0100

    AKS url

--- .ibm/pipelines/jobs/aks-helm.sh
+++ .ibm/pipelines/jobs/aks-helm.sh
@@ -8,22 +8,21 @@ source "$DIR"/cluster/aks/aks-helm-deployment.sh
 source "$DIR"/cluster/aks/az.sh
 
 handle_aks_helm() {
-  echo "Starting AKS deployment"
+  echo "Starting AKS Helm deployment"
 
-  export K8S_CLUSTER_ROUTER_BASE=$(kubectl get svc nginx --namespace app-routing-system -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-  export NAME_SPACE_K8S="showcase-k8s-ci-nightly"
-  export NAME_SPACE_RBAC_K8S="showcase-rbac-k8s-ci-nightly"
+  K8S_CLUSTER_ROUTER_BASE=$(kubectl get svc nginx --namespace app-routing-system -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+  NAME_SPACE_K8S="showcase-k8s-ci-nightly"
+  NAME_SPACE_RBAC_K8S="showcase-rbac-k8s-ci-nightly"
+  export K8S_CLUSTER_ROUTER_BASE NAME_SPACE_K8S NAME_SPACE_RBAC_K8S
 
-  url="https://${K8S_CLUSTER_ROUTER_BASE}"
 
   cluster_setup_k8s_helm
 
   initiate_aks_helm_deployment
-  check_and_test "${RELEASE_NAME}" "${NAME_SPACE_K8S}" "${url}" 50 30 50
+  check_and_test "${RELEASE_NAME}" "${NAME_SPACE_K8S}" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30 50
   delete_namespace "${NAME_SPACE_K8S}"
+
   initiate_rbac_aks_helm_deployment
-  local rbac_rhdh_base_url="https://${K8S_CLUSTER_ROUTER_BASE}"
-  initiate_rbac_aks_helm_deployment
-  check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC_K8S}" "${rbac_rhdh_base_url}" 50 30 50
+  check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC_K8S}" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30 50
   delete_namespace "${NAME_SPACE_RBAC_K8S}"
 }
