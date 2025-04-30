6575048bcbed90a5da0a00bdd202d4ce102a9130
Gustavo Lira <guga.java@gmail.com>
2025-04-30 15:05:35 -0300

    RHIDP-6618 Update scaffold tests: refine event log validation and improve test descriptions
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>

--- e2e-tests/playwright/e2e/audit-log/scaffold.spec.ts
+++ e2e-tests/playwright/e2e/audit-log/scaffold.spec.ts
@@ -12,12 +12,10 @@ test.describe("Audit Log check for Catalog Plugin", () => {
   const template =
     "https://github.com/RoadieHQ/sample-service/blob/main/demo_template.yaml";
 
-  // Login to OpenShift before all tests
   test.beforeAll(async () => {
     await LogUtils.loginToOpenShift();
   });
 
-  // Common setup before each test
   test.beforeEach(async ({ page }) => {
     uiHelper = new UIhelper(page);
     common = new Common(page);
@@ -26,25 +24,7 @@ test.describe("Audit Log check for Catalog Plugin", () => {
     await page.goto("/create");
   });
 
-  // /**
-  //  * Helper function to validate log events for Scaffolder Plugin
-  //  */
-  // async function validateScaffolderLogEvent(
-  //   eventId: string,
-  //   request: LogRequest,
-  // ) {
-  //   await LogUtils.validateLogEvent(
-  //     eventId,
-  //     "user:development/guest",
-  //     request,
-  //     undefined,
-  //     undefined,
-  //     "succeeded",
-  //     "scaffolder",
-  //   );
-  // }
-
-  test("Should fetch logs for 'template-parameter-schema' event and validate log structure and values", async () => {
+  test("Should fetch logs for 'entity-mutate or location-mutate' event and validate log structure and values", async () => {
     await uiHelper.clickButton("Register Existing Component");
     const isComponentIsAlreadyRegistered =
       await catalogImport.registerExistingComponent(template, false);
