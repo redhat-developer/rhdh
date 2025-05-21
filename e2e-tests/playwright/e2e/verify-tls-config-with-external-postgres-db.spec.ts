3cfb08f96632af7f6565a3910090bb723bfaaea7
Alessandro Barbarossa <abarbaro@redhat.com>
2025-05-21 14:49:37 +0200

    enable postgres operator again

--- e2e-tests/playwright/e2e/verify-tls-config-with-external-postgres-db.spec.ts
+++ e2e-tests/playwright/e2e/verify-tls-config-with-external-postgres-db.spec.ts
@@ -3,7 +3,7 @@ import { UIhelper } from "../utils/ui-helper";
 import { Common } from "../utils/common";
 
 // TODO: # Remove skip when https://issues.redhat.com/browse/RHIDP-7604 is fixed.
-test.describe.skip("Verify TLS configuration with external Postgres DB", () => {
+test.describe("Verify TLS configuration with external Postgres DB", () => {
   test("Verify successful DB connection and display of expected entities in the Home Page and Catalog", async ({
     page,
   }) => {
