48adfbd4eecbc2a045277688af1bcb0885280f2c
Gustavo Lira <guga.java@gmail.com>
2025-05-30 13:58:14 -0300

    tes

--- .ibm/pipelines/jobs/gke-helm.sh
+++ .ibm/pipelines/jobs/gke-helm.sh
@@ -32,11 +32,11 @@ handle_gke_helm() {
 
   cluster_setup_k8s_helm
 
-  initiate_gke_helm_deployment
-  check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30
-  delete_namespace "${NAME_SPACE}"
+  # initiate_gke_helm_deployment
+  # check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30
+  # delete_namespace "${NAME_SPACE}"
 
   initiate_rbac_gke_helm_deployment
   check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30
-  delete_namespace "${NAME_SPACE_RBAC}"
+  # delete_namespace "${NAME_SPACE_RBAC}"
 }
