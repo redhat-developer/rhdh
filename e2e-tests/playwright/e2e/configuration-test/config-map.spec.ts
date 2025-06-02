40e6f7433cd57cd803bba2c02f510a82af1c3d49
Gustavo Lira e Silva <guga.java@gmail.com>
2025-06-02 11:48:17 -0300

    chore(e2e): rhidp-7172 Plugin 'permission' fails during startup (#2862)
    * RHIDP-7172 Plugin 'permission' fails during startup
    
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>
    
    * RHIDP-7172 Plugin 'permission' fails during startup
    
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>
    
    * Refactor ConfigMap handling and increase timeouts in tests
    
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>
    
    * RHIDP-7559 Adapt Auditor check for RBAC Plugin for Kubernetes
    
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>
    
    * test
    
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>
    
    * Remove unused UIhelper instantiation in config-map.spec.ts
    
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>
    
    * Enhance database cleanup script with error handling and logging
    
    - Added connection error handling when retrieving database list.
    - Included checks for empty database lists and improved logging for database drop attempts.
    - Used "DROP DATABASE IF EXISTS" to prevent errors if a database does not exist.
    
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>
    
    * RHIDP-7172 Remove visibility check for search bar in configuration test
    
    * Remove clear_database call from utils.sh and add it to periodic.sh
    
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>
    
    * RHIDP-7172 Update timeout settings in e2e tests and add TODO for TLS health check skip
    
    - Reduced timeout in config-map.spec.ts from 15 minutes to 5 minutes.
    - Updated KubeClient timeout from 10 minutes to 5 minutes.
    - Added a TODO comment to the TLS health check test to remove the skip after addressing the related issue RHIDP-7604.
    
    * RHIDP-7172 Fix KubeClient configMap filename to use dynamic name instead of hardcoded 'app-config.yaml'
    
    * Update TLS health check test to include a TODO for future removal of skip
    
    ---------
    
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>

--- e2e-tests/playwright/e2e/configuration-test/config-map.spec.ts
+++ e2e-tests/playwright/e2e/configuration-test/config-map.spec.ts
@@ -4,19 +4,19 @@ import { LOGGER } from "../../utils/logger";
 import { Common } from "../../utils/common";
 import { UIhelper } from "../../utils/ui-helper";
 
-test.describe.skip("Change app-config at e2e test runtime", () => {
+test.describe("Change app-config at e2e test runtime", () => {
   test("Verify title change after ConfigMap modification", async ({ page }) => {
-    test.setTimeout(300000); // Increasing to 5 minutes
-
-    const configMapName = "rhdh-backstage-app-config";
+    test.setTimeout(300000); // 5 minutes
+    const configMapName = "app-config";
     const namespace = process.env.NAME_SPACE_RUNTIME || "showcase-runtime";
     const deploymentName = "rhdh-backstage";
 
     const kubeUtils = new KubeClient();
     const dynamicTitle = generateDynamicTitle();
-    const uiHelper = new UIhelper(page);
     try {
-      LOGGER.info(`Updating ConfigMap '${configMapName}' with new title.`);
+      LOGGER.info(
+        `Updating ConfigMap '${configMapName}' with new title: ${dynamicTitle}`,
+      );
       await kubeUtils.updateConfigMapTitle(
         configMapName,
         namespace,
@@ -28,15 +28,13 @@ test.describe.skip("Change app-config at e2e test runtime", () => {
       );
       await kubeUtils.restartDeployment(deploymentName, namespace);
 
+      LOGGER.info("Deployment restarted successfully. Loading page...");
       const common = new Common(page);
       await page.context().clearCookies();
       await page.context().clearPermissions();
       await page.reload({ waitUntil: "domcontentloaded" });
       await common.loginAsGuest();
       await new UIhelper(page).openSidebar("Home");
-      await uiHelper.verifyHeading("Welcome back!");
-      await uiHelper.verifyText("Quick Access");
-      await expect(page.locator("#search-bar-text-field")).toBeVisible();
       LOGGER.info("Verifying new title in the UI...");
       expect(await page.title()).toContain(dynamicTitle);
       LOGGER.info("Title successfully verified in the UI.");
