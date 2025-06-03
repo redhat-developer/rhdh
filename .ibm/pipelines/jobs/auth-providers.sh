d81f07c8570f24df8446b6b0f8d120818a0646b1
albarbaro <abarbaro@redhat.com>
2025-06-03 10:55:29 +0200

    refactor rhdh auth-providers job

--- .ibm/pipelines/jobs/auth-providers.sh
+++ .ibm/pipelines/jobs/auth-providers.sh
@@ -2,35 +2,14 @@
 
 # shellcheck source=.ibm/pipelines/utils.sh
 source "$DIR"/utils.sh
-
-install_rhdh_operator() {
-  local dir=$1
-  local namespace=$2
-
-  configure_namespace "$namespace"
-
-  if [[ -z "${IS_OPENSHIFT}" || "${IS_OPENSHIFT,,}" == "false" ]]; then
-    setup_image_pull_secret "rhdh-operator" "rh-pull-secret" "${REGISTRY_REDHAT_IO_SERVICE_ACCOUNT_DOCKERCONFIGJSON}"
-  fi
-  # Make sure script is up to date
-  rm -f /tmp/install-rhdh-catalog-source.sh
-  curl -L "https://raw.githubusercontent.com/redhat-developer/rhdh-operator/refs/heads/${RELEASE_BRANCH_NAME}/.rhdh/scripts/install-rhdh-catalog-source.sh" > /tmp/install-rhdh-catalog-source.sh
-  chmod +x /tmp/install-rhdh-catalog-source.sh
-  if [[ "$RELEASE_BRANCH_NAME" == "main" ]]; then
-    echo "Installing RHDH operator with '--next' flag"
-    bash -x /tmp/install-rhdh-catalog-source.sh --next --install-operator rhdh
-  else
-    local operator_version="${RELEASE_BRANCH_NAME#release-}"
-    echo "Installing RHDH operator with '-v $operator_version' flag"
-    bash -x /tmp/install-rhdh-catalog-source.sh -v "$operator_version" --install-operator rhdh
-  fi
-}
+source "$DIR"/install-methods/operator.sh
 
 handle_auth_providers() {
-  
+  local retry_operator_installation="${1:-1}"
   oc_login
   configure_namespace "${OPERATOR_MANAGER}"
-  install_rhdh_operator "${DIR}" "${OPERATOR_MANAGER}"
+  install_rhdh_operator "${DIR}" "${OPERATOR_MANAGER}" "$retry_operator_installation"
+  wait_for_backstage_crd "default"
 
   export K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
 
