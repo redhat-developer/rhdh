4cd0c0207f98be2c610d0eef69ac8520700993f4
Alessandro Barbarossa <abarbaro@redhat.com>
2025-06-03 08:21:54 +0200

    Update .ibm/pipelines/utils.sh
    Co-authored-by: Zbyněk Drápela <61500440+zdrapela@users.noreply.github.com>

--- .ibm/pipelines/utils.sh
+++ .ibm/pipelines/utils.sh
@@ -359,7 +359,7 @@ check_operator_status() {
 # Installs the Crunchy Postgres Operator using predefined parameters
 install_crunchy_postgres_operator(){
   install_subscription postgresql openshift-operators postgresql v5 certified-operators
-  check_operator_status 300 "openshift-operators" " ostgres for Kubernetes" "Succeeded"
+  check_operator_status 300 "openshift-operators" "Postgres for Kubernetes" "Succeeded"
 }
 
 add_helm_repos() {
