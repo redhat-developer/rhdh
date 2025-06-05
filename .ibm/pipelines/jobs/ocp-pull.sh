a4c0e498eee6cd7063736f601f25ce73989da48c
Omar Al Jaljuli <oaljalju@redhat.com>
2025-06-05 00:52:00 -0400

    PR clean up
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- .ibm/pipelines/jobs/ocp-pull.sh
+++ .ibm/pipelines/jobs/ocp-pull.sh
@@ -8,9 +8,9 @@ handle_ocp_pull() {
   export K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
   cluster_setup
   initiate_deployments
-  # deploy_test_backstage_customization_provider "${NAME_SPACE}"
-  # local url="https://${RELEASE_NAME}-developer-hub-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
-  # check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "${url}"
+  deploy_test_backstage_customization_provider "${NAME_SPACE}"
+  local url="https://${RELEASE_NAME}-developer-hub-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
+  check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "${url}"
   local rbac_url="https://${RELEASE_NAME_RBAC}-developer-hub-${NAME_SPACE_RBAC}.${K8S_CLUSTER_ROUTER_BASE}"
   check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "${rbac_url}"
 }
