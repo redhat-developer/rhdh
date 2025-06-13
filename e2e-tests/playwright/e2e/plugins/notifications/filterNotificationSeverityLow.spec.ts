cd195103f1fdbc19d72e7b7997167604cfe51d33
Thomas Stetson <tstetson@redhat.com>
2025-06-13 15:44:46 -0400

    add more orchestrator and notification tests; needs work

--- e2e-tests/playwright/e2e/plugins/notifications/filterNotificationSeverityLow.spec.ts
+++ e2e-tests/playwright/e2e/plugins/notifications/filterNotificationSeverityLow.spec.ts
@@ -4,32 +4,29 @@ import { Common } from "../../../utils/common";
 import RhdhNotficationsApi from "../../../support/api/notifications";
 import { Notifications } from "../../../support/api/notifications-api-structures";
 import { RhdhAuthApiHack } from "../../../support/api/rhdh-auth-api-hack";
-
+import { Orchestrator } from "../../../support/pages/orchestrator";
 import { NotificationPage } from "../../../support/pages/notifications";
 
-test.describe("Filter critical notification tests", () => {
+test.describe("Filter low notification tests", () => {
   let uiHelper: UIhelper;
   let common: Common;
+  let orchestrator: Orchestrator;
   let notificationPage: NotificationPage;
-
-  test.beforeAll(async () => {
-    test.info().annotations.push({
-      type: "component",
-      description: "integration",
-    });
-  });
+  let apiToken: string;
 
   test.beforeEach(async ({ page }) => {
     uiHelper = new UIhelper(page);
     common = new Common(page);
+    orchestrator = new Orchestrator(page);
     notificationPage = new NotificationPage(page);
     await common.loginAsKeycloakUser();
-    await RhdhAuthApiHack.getToken(page);
+    apiToken = await RhdhAuthApiHack.getToken(page);
+    console.log(`apiToken -> ${apiToken}`)
   });
 
-  test("Filter notifications by severity - critical", async () => {
+  test("Filter notifications by severity - low", async () => {
     const r = (Math.random() + 1).toString(36).substring(7);
-    const severity = "critical";
+    const severity = "low";
     const notificationsApi = await RhdhNotficationsApi.build("test-token");
     // Used broadcast here, but we should use type: entity and entityRef: ["user:<namespace>/<username>"]
     const notification: Notifications = {
@@ -46,7 +43,7 @@ test.describe("Filter critical notification tests", () => {
     };
     await notificationsApi.createNotification(notification);
     await uiHelper.openSidebar("Notifications");
-    await notificationPage.selectSeverity("Critical");
+    await notificationPage.selectSeverity("Low");
     await notificationPage.notificationContains(
       `UI Notification Mark all as read ${severity}-${r}`,
     );
