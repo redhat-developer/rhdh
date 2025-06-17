9e388d5b7304ab44628858b57cd00f79cc67fda1
guy oron <guyoron@mail.tau.ac.il>
2025-06-17 10:15:47 +0300

    feat(e2e): add tests for GitHub PR statistics and security insights plugins - Created Playwright spec files for GitHub PR statistics and security insights plugins - Enabled the plugins in values_showcase.yaml - Added new GitHub sign-in logic using popup modal

---
+++ e2e-tests/playwright/e2e/plugins/github-security-insights.spec.ts
@@ -0,0 +1,41 @@
+import { test } from "@playwright/test";
+import { UIhelper } from "../../utils/ui-helper";
+import { Common } from "../../utils/common";
+
+// if (!process.env.GH_USER_ID || !process.env.GH_USER_PASS || !process.env.GH_2FA_SECRET) {
+//     throw new Error("GH_USER_ID, GH_USER_PASS, and GH_2FA_SECRET must be set in your environment or .env file.");
+// }
+
+test("Verify GitHub Security Insights plugin after login", async ({
+  page,
+  context,
+}) => {
+  const repoName = "Backstage Showcase";
+  const common = new Common(page);
+  const uiHelper = new UIhelper(page);
+
+  await common.loginAsKeycloakUser(
+    process.env.GH_USER_ID,
+    process.env.GH_USER_PASS,
+  );
+  await uiHelper.openSidebar("Catalog");
+  await uiHelper.searchInputPlaceholder(repoName);
+  await uiHelper.verifyLink(repoName);
+  await uiHelper.clickLink(repoName);
+
+  await uiHelper.clickBtnInCard("Dependabot Alerts", "Sign in", true);
+  await uiHelper.isBtnVisible("Log in");
+  await Promise.all([
+    common.githubLoginPopUpModal(
+      context,
+      process.env.GH_USER_ID,
+      process.env.GH_USER_PASS,
+      process.env.GH_2FA_SECRET,
+    ),
+    uiHelper.clickButton("Log in"),
+  ]);
+  await uiHelper.verifyTextinCard("Dependabot Alerts", "Critical severity");
+  await uiHelper.verifyTextinCard("Dependabot Alerts", "High severity");
+  await uiHelper.verifyTextinCard("Dependabot Alerts", "Medium severity");
+  await uiHelper.verifyTextinCard("Dependabot Alerts", "Low severity");
+});
