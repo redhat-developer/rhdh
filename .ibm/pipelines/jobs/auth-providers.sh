73c1dedb7384189348dfdf83afa8323388c26869
albarbaro <abarbaro@redhat.com>
2025-06-17 12:06:46 +0200

    fix operator setup

--- .ibm/pipelines/jobs/auth-providers.sh
+++ .ibm/pipelines/jobs/auth-providers.sh
@@ -2,14 +2,20 @@
 
 # shellcheck source=.ibm/pipelines/utils.sh
 source "$DIR"/utils.sh
-source "$DIR"/install-methods/operator.sh
+source "$DIR"/jobs/operator.sh
 
 handle_auth_providers() {
   local retry_operator_installation="${1:-1}"
   oc_login
   configure_namespace "${OPERATOR_MANAGER}"
   install_rhdh_operator "${DIR}" "${OPERATOR_MANAGER}" "$retry_operator_installation"
-  wait_for_backstage_crd "default"
+  timeout 300 bash -c '
+    while ! oc get crd/backstages.rhdh.redhat.com -n "${namespace}" >/dev/null 2>&1; do
+        echo "Waiting for Backstage CRD to be created..."
+        sleep 20
+    done
+    echo "Backstage CRD is created."
+    ' || echo "Error: Timed out waiting for Backstage CRD creation."
 
   export K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
 
