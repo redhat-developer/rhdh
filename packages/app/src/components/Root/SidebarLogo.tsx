67e1f593d7c9384fe870fbb94d1fea818b396d95
logonoff <git@logonoff.co>
2025-06-19 16:04:39 -0400

    fix(SidebarLogo): support both light and dark variants

--- packages/app/src/components/Root/SidebarLogo.tsx
+++ packages/app/src/components/Root/SidebarLogo.tsx
@@ -1,6 +1,8 @@
 import { Link, useSidebarOpenState } from '@backstage/core-components';
 import { configApiRef, useApi } from '@backstage/core-plugin-api';
 
+import { useTheme } from '@mui/material/styles';
+import type { ThemeConfig } from '@red-hat-developer-hub/backstage-plugin-theme';
 import { makeStyles } from 'tss-react/mui';
 
 import LogoFull from './LogoFull';
@@ -33,20 +35,55 @@ const LogoRender = ({
   );
 };
 
+type FullLogo =
+  | {
+      dark: string;
+      light: string;
+    }
+  | string
+  | undefined;
+
+/**
+ * Gets a themed image based on the current theme.
+ */
+const useThemedImage = (key: string) => {
+  const theme = useTheme();
+
+  const appBarBackgroundScheme =
+    (theme as ThemeConfig)?.palette?.rhdh?.general?.appBarBackgroundScheme ??
+    'dark';
+
+  const configApi = useApi(configApiRef);
+
+  /** The fullLogo config specified by Red Hat Developer Hub */
+  const fullLogo = configApi.getOptional<FullLogo>(key);
+
+  /** The dark theme full logo config */
+  const darkLogoFullBase64URI =
+    typeof fullLogo === 'string' ? undefined : fullLogo?.dark;
+
+  /** The light theme full logo config */
+  const lightLogoFullBase64URI =
+    typeof fullLogo === 'string' ? fullLogo : fullLogo?.light;
+
+  return appBarBackgroundScheme === 'dark'
+    ? darkLogoFullBase64URI
+    : lightLogoFullBase64URI;
+};
+
 export const SidebarLogo = () => {
   const { classes } = useStyles();
   const { isOpen } = useSidebarOpenState();
+
   const configApi = useApi(configApiRef);
-  const logoFullBase64URI = configApi.getOptionalString(
-    'app.branding.fullLogo',
-  );
+
+  const logoFullBase64URI = useThemedImage('app.branding.fullLogo');
+
   const fullLogoWidth = configApi
     .getOptional('app.branding.fullLogoWidth')
     ?.toString();
 
-  const logoIconBase64URI = configApi.getOptionalString(
-    'app.branding.iconLogo',
-  );
+  const logoIconBase64URI = useThemedImage('app.branding.iconLogo');
 
   return (
     <div className={classes.sidebarLogo}>
