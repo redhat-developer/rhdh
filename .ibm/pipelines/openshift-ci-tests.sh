551ecca6c33c7db1b44b43d3d2428ce1f39e2907
Gustavo Lira <guga.java@gmail.com>
2025-06-20 18:13:36 -0300

    test [skip-build]
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>

--- .ibm/pipelines/openshift-ci-tests.sh
+++ .ibm/pipelines/openshift-ci-tests.sh
@@ -19,29 +19,30 @@ export OVERALL_RESULT
 
 # Define a cleanup function to be executed upon script exit.
 # shellcheck disable=SC2317
-cleanup() {
-  if [[ $? -ne 0 ]]; then
+#cleanup() {
+#  if [[ $? -ne 0 ]]; then
+#
+#    echo "Exited with an error, setting OVERALL_RESULT to 1"
+#    save_overall_result 1
+#  fi
+#  echo "Cleaning up before exiting"
+#  if [[ "${OPENSHIFT_CI}" == "true" ]]; then
+#    case "$JOB_NAME" in
+#      *gke*)
+#        echo "Calling cleanup_gke"
+#        cleanup_gke
+#        ;;
+#    esac
+#  fi
+#  rm -rf ~/tmpbin
+#}
 
-    echo "Exited with an error, setting OVERALL_RESULT to 1"
-    save_overall_result 1
-  fi
-  echo "Cleaning up before exiting"
-  if [[ "${OPENSHIFT_CI}" == "true" ]]; then
-    case "$JOB_NAME" in
-      *gke*)
-        echo "Calling cleanup_gke"
-        cleanup_gke
-        ;;
-    esac
-  fi
-  rm -rf ~/tmpbin
-}
-
-trap cleanup EXIT INT ERR
+#trap cleanup EXIT INT ERR
 
 SCRIPTS=(
   "utils.sh"
   "env_variables.sh"
+  "clear-database.sh"
 )
 
 # Source explicitly specified scripts
