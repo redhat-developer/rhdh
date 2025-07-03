3d259d8123fe22c1a1cba1357c2f6e93653b1099
Gustavo Lira <guga.java@gmail.com>
2025-07-03 14:35:39 -0300

    RHIDP-7981 [Test Automation] Fix sanity-plugins nightly tests
    Signed-off-by: Gustavo Lira <guga.java@gmail.com>

--- .ibm/pipelines/openshift-ci-tests.sh
+++ .ibm/pipelines/openshift-ci-tests.sh
@@ -19,25 +19,25 @@ export OVERALL_RESULT
 
 # Define a cleanup function to be executed upon script exit.
 # shellcheck disable=SC2317
-#cleanup() {
-#  if [[ $? -ne 0 ]]; then
-#
-#    echo "Exited with an error, setting OVERALL_RESULT to 1"
-#    save_overall_result 1
-#  fi
-#  echo "Cleaning up before exiting"
-#  if [[ "${OPENSHIFT_CI}" == "true" ]]; then
-#    case "$JOB_NAME" in
-#      *gke*)
-#        echo "Calling cleanup_gke"
-#        cleanup_gke
-#        ;;
-#    esac
-#  fi
-#  rm -rf ~/tmpbin
-#}
-#
-#trap cleanup EXIT INT ERR
+cleanup() {
+  if [[ $? -ne 0 ]]; then
+
+    echo "Exited with an error, setting OVERALL_RESULT to 1"
+    save_overall_result 1
+  fi
+  echo "Cleaning up before exiting"
+  if [[ "${OPENSHIFT_CI}" == "true" ]]; then
+    case "$JOB_NAME" in
+      *gke*)
+        echo "Calling cleanup_gke"
+        cleanup_gke
+        ;;
+    esac
+  fi
+  rm -rf ~/tmpbin
+}
+
+trap cleanup EXIT INT ERR
 
 SCRIPTS=(
   "utils.sh"
@@ -106,7 +106,6 @@ main() {
   esac
 
   echo "Main script completed with result: ${OVERALL_RESULT}"
-  sleep 3600
   exit "${OVERALL_RESULT}"
 
 }
