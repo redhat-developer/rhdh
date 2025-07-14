8e6a8f12a5cb2ad75c17e836215ac2ba01db846a
Omar Al Jaljuli <oaljalju@redhat.com>
2025-07-14 12:31:56 -0400

    reinclude cleanup fn
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- .ibm/pipelines/openshift-ci-tests.sh
+++ .ibm/pipelines/openshift-ci-tests.sh
@@ -10,20 +10,20 @@ OVERALL_RESULT=0
 
 # Define a cleanup function to be executed upon script exit.
 # shellcheck disable=SC2317
-# cleanup() {
-#   echo "Cleaning up before exiting"
-#   if [[ "${OPENSHIFT_CI}" == "true" ]]; then
-#     case "$JOB_NAME" in
-#       *gke*)
-#         echo "Calling cleanup_gke"
-#         cleanup_gke
-#         ;;
-#     esac
-#   fi
-#   rm -rf ~/tmpbin
-# }
+cleanup() {
+  echo "Cleaning up before exiting"
+  if [[ "${OPENSHIFT_CI}" == "true" ]]; then
+    case "$JOB_NAME" in
+      *gke*)
+        echo "Calling cleanup_gke"
+        cleanup_gke
+        ;;
+    esac
+  fi
+  rm -rf ~/tmpbin
+}
 
-# trap cleanup EXIT INT ERR
+trap cleanup EXIT INT ERR
 
 SCRIPTS=(
     "env_variables.sh"
