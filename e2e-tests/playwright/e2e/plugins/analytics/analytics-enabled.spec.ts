a98c545e750ba674da077d31272050491d79da6a
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-08-26 16:39:28 +0200

    Cherry-pick linter

--- e2e-tests/playwright/e2e/plugins/analytics/analytics-enabled.spec.ts
+++ e2e-tests/playwright/e2e/plugins/analytics/analytics-enabled.spec.ts
@@ -12,18 +12,19 @@ test.describe('Check "analytics-provider-segment" plugin is enabled', () => {
   ]);
 
   test('Check "analytics-provider-segment" plugin is enabled', async () => {
-  const analytics = new Analytics();
-  const api = new APIHelper();
+    const analytics = new Analytics();
+    const api = new APIHelper();
 
-  // This test uses the Guest token to check the loaded plugins.
-  // Static token is not allowed to list the plugins.
-  // If this breaks, we can use RhdhAuthApiHack to get the User token.
-  const authHeader = await api.getGuestAuthHeader();
-  const pluginsList = await analytics.getLoadedDynamicPluginsList(authHeader);
-  const isPluginListed = analytics.checkPluginListed(
-    pluginsList,
-    "backstage-community-plugin-analytics-provider-segment",
-  );
+    // This test uses the Guest token to check the loaded plugins.
+    // Static token is not allowed to list the plugins.
+    // If this breaks, we can use RhdhAuthApiHack to get the User token.
+    const authHeader = await api.getGuestAuthHeader();
+    const pluginsList = await analytics.getLoadedDynamicPluginsList(authHeader);
+    const isPluginListed = analytics.checkPluginListed(
+      pluginsList,
+      "backstage-community-plugin-analytics-provider-segment",
+    );
 
-  expect(isPluginListed).toBe(true);
+    expect(isPluginListed).toBe(true);
+  });
 });
