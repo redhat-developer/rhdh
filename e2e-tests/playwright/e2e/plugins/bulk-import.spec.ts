5eadce1c78c4743f009b0e5ef038101f8b299fd2
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-09-03 17:07:58 +0200

    serial

--- e2e-tests/playwright/e2e/plugins/bulk-import.spec.ts
+++ e2e-tests/playwright/e2e/plugins/bulk-import.spec.ts
@@ -10,8 +10,7 @@ import {
 } from "../../support/test-data/bulk-import";
 
 // Pre-req : plugin-bulk-import & plugin-bulk-import-backend-dynamic
-test.describe("Bulk Import plugin", () => {
-  test.describe.configure({ retries: process.env.CI ? 2 : 0, mode: "serial" });
+test.describe.serial("Bulk Import plugin", () => {
   test.skip(() => process.env.JOB_NAME.includes("osd-gcp")); // skipping due to RHIDP-5704 on OSD Env
 
   let page: Page;
