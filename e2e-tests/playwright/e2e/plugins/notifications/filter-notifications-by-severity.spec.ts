87b412c0923cf97e1514c44afe11c4721a7d5f67
Thomas Stetson <tstetson@redhat.com>
2025-09-09 11:10:35 -0400

    refactor(e2e-tests): couple more linter issues

--- e2e-tests/playwright/e2e/plugins/notifications/filter-notifications-by-severity.spec.ts
+++ e2e-tests/playwright/e2e/plugins/notifications/filter-notifications-by-severity.spec.ts
@@ -4,22 +4,19 @@ import { Common } from "../../../utils/common";
 import RhdhNotficationsApi from "../../../support/api/notifications";
 import { Notifications } from "../../../support/api/notifications-api-structures";
 import { RhdhAuthApiHack } from "../../../support/api/rhdh-auth-api-hack";
-import { Orchestrator } from "../../../support/pages/orchestrator";
 import { NotificationPage } from "../../../support/pages/notifications";
 
 test.describe("Filter critical notification tests", () => {
   let uiHelper: UIhelper;
   let common: Common;
-  let orchestrator: Orchestrator;
   let notificationPage: NotificationPage;
   let apiToken: string;
 
-  let severities = ["Critical", "High", "Normal", "Low"];
+  const severities = ["Critical", "High", "Normal", "Low"];
 
   test.beforeEach(async ({ page }) => {
     uiHelper = new UIhelper(page);
     common = new Common(page);
-    orchestrator = new Orchestrator(page);
     notificationPage = new NotificationPage(page);
     await common.loginAsKeycloakUser();
     apiToken = await RhdhAuthApiHack.getToken(page);
