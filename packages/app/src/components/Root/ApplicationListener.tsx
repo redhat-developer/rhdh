76c71cc217fa25ffcb735c88de4c020e114e72cc
Yi Cai <yicai@redhat.com>
2025-09-08 22:14:36 -0400

    fixed lint issue
    Signed-off-by: Yi Cai <yicai@redhat.com>

--- packages/app/src/components/Root/ApplicationListener.tsx
+++ packages/app/src/components/Root/ApplicationListener.tsx
@@ -20,37 +20,37 @@ class ErrorBoundary extends Component<
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    const { Component } = this.props;
-    const name = Component.displayName ?? Component.name ?? 'Component';
+    const { Component: Comp } = this.props;
+    const name = Comp.displayName ?? Comp.name ?? 'Component';
     // eslint-disable-next-line no-console
     console.error(`Error in application/listener ${name}: ${error.message}`, {
       error,
       errorInfo,
-      Component,
+      Component: Comp,
     });
   }
 
   render() {
-    const { Component } = this.props;
+    const { Component: Comp } = this.props;
     const { error } = this.state;
     if (error) {
-      const name = Component.displayName ?? Component.name ?? 'Component';
+      const name = Comp.displayName ?? Comp.name ?? 'Component';
       const title = `Error in application/listener ${name}: ${error.message}`;
       return <ErrorPanel title={title} error={error} />;
     }
-    return <Component />;
+    return <Comp />;
   }
 }
 
 export const ApplicationListener = () => {
   const { mountPoints } = useContext(DynamicRootContext);
   const listeners = mountPoints['application/listener'] ?? [];
-  return listeners.map(({ Component }, index) => {
+  return listeners.map(({ Component: Comp }, index) => {
     return (
       <ErrorBoundary
         // eslint-disable-next-line react/no-array-index-key
         key={index}
-        Component={Component}
+        Component={Comp}
       />
     );
   });
