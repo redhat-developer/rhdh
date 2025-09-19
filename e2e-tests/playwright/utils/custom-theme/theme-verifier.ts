9c9f8678e0eaf59938e68f19d0ce8d1b405819c4
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-09-19 08:08:53 +0200

    Update theme-verifier.ts

--- e2e-tests/playwright/utils/custom-theme/theme-verifier.ts
+++ e2e-tests/playwright/utils/custom-theme/theme-verifier.ts
@@ -14,6 +14,8 @@ export class ThemeVerifier {
   async setTheme(theme: "Light" | "Dark" | "Light Dynamic" | "Dark Dynamic") {
     await this.uiHelper.goToPageUrl("/settings", "Settings");
     await this.uiHelper.clickBtnByTitleIfNotPressed(`Select theme ${theme}`);
+    const themeButton = this.page.locator(`button:has-text("${theme}")`);
+    await expect(themeButton).toHaveAttribute("aria-pressed", "true");
   }
 
   async verifyHeaderGradient(expectedGradient: string) {
