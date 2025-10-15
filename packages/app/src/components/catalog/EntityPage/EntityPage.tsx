9f0f742c94b6bc702448ae03ed537e648e667698
its-mitesh-kumar <itsmiteshkumar98@gmail.com>
2025-10-15 20:06:12 +0530

    passing trnaslated tab component
    Signed-off-by: its-mitesh-kumar <itsmiteshkumar98@gmail.com>

--- packages/app/src/components/catalog/EntityPage/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage/EntityPage.tsx
@@ -1,44 +1,7 @@
-import { useTranslation } from '../../../hooks/useTranslation';
 import { ContextMenuAwareEntityLayout } from './ContextMenuAwareEntityLayout';
 import { tabChildren, tabRules } from './defaultTabs';
 import { dynamicEntityTab, DynamicEntityTabProps } from './DynamicEntityTab';
 import { mergeTabs } from './utils';
-
-const EntityPageWithTranslation = ({
-  entityTabOverrides,
-}: {
-  entityTabOverrides: Record<
-    string,
-    Omit<DynamicEntityTabProps, 'path' | 'if' | 'children'>
-  >;
-}) => {
-  const { t } = useTranslation();
-
-  const getTranslatedTitle = (title: string, titleKey?: string) => {
-    if (!titleKey) {
-      return title;
-    }
-    const translatedTitle = t(titleKey as any, {});
-    if (translatedTitle !== titleKey) {
-      return translatedTitle;
-    }
-    return title;
-  };
-
-  return (
-    <ContextMenuAwareEntityLayout>
-      {mergeTabs(entityTabOverrides).map(([path, config]) => {
-        return dynamicEntityTab({
-          ...config,
-          title: getTranslatedTitle(config.title, config.titleKey),
-          path,
-          ...(tabRules[path] ? tabRules[path] : {}),
-          ...(tabChildren[path] ? tabChildren[path] : {}),
-        } as DynamicEntityTabProps);
-      })}
-    </ContextMenuAwareEntityLayout>
-  );
-};
 
 /**
  * Displays the tabs and content for a catalog entity
@@ -51,5 +14,16 @@ export const entityPage = (
     Omit<DynamicEntityTabProps, 'path' | 'if' | 'children'>
   > = {},
 ) => {
-  return <EntityPageWithTranslation entityTabOverrides={entityTabOverrides} />;
+  return (
+    <ContextMenuAwareEntityLayout>
+      {mergeTabs(entityTabOverrides).map(([path, config]) => {
+        return dynamicEntityTab({
+          ...config,
+          path,
+          ...(tabRules[path] ? tabRules[path] : {}),
+          ...(tabChildren[path] ? tabChildren[path] : {}),
+        } as DynamicEntityTabProps);
+      })}
+    </ContextMenuAwareEntityLayout>
+  );
 };
