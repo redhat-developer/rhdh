43c87eaad20656691c247b5b7f949a3af4630266
Gustavo Lira <guga.java@gmail.com>
2025-10-21 08:51:47 -0300

    RHIDP-9235 style: format configuration files

--- .ibm/pipelines/utils.sh
+++ .ibm/pipelines/utils.sh
@@ -589,7 +589,6 @@ uninstall_olm() {
   fi
 }
 
-
 # Installs the Red Hat OpenShift Pipelines operator if not already installed
 install_pipelines_operator() {
   DISPLAY_NAME="Red Hat OpenShift Pipelines"
