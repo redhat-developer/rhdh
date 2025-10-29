ce2b4987191386c2657a755e16c36fba5d7f9f80
Gustavo Lira <guga.java@gmail.com>
2025-10-29 19:09:53 -0300

    test 1.8 [skip-build]

--- .ibm/pipelines/jobs/ocp-pull.sh
+++ .ibm/pipelines/jobs/ocp-pull.sh
@@ -9,9 +9,9 @@ handle_ocp_pull() {
   export K8S_CLUSTER_ROUTER_BASE
   cluster_setup_ocp_helm
   initiate_deployments
-  deploy_test_backstage_customization_provider "${NAME_SPACE}"
+  # deploy_test_backstage_customization_provider "${NAME_SPACE}"
   local url="https://${RELEASE_NAME}-developer-hub-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
   check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "${url}"
-  local rbac_url="https://${RELEASE_NAME_RBAC}-developer-hub-${NAME_SPACE_RBAC}.${K8S_CLUSTER_ROUTER_BASE}"
-  check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "${rbac_url}"
+  # local rbac_url="https://${RELEASE_NAME_RBAC}-developer-hub-${NAME_SPACE_RBAC}.${K8S_CLUSTER_ROUTER_BASE}"
+  # check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "${rbac_url}"
 }
