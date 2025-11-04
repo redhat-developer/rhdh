aad31ee19664e1dd4fd933dd90e76d4e41a689a3
rhdh-bot service account <rhdh-bot@redhat.com>
2025-11-04 09:58:34 -0500

    Update ocp-pull.sh

--- .ibm/pipelines/jobs/ocp-pull.sh
+++ .ibm/pipelines/jobs/ocp-pull.sh
@@ -9,10 +9,8 @@ handle_ocp_pull() {
   cluster_setup_ocp_helm
   initiate_deployments
   deploy_test_backstage_customization_provider "${NAME_SPACE}"
-  # local url="https://${RELEASE_NAME}-developer-hub-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
-  local url="https://$(oc get route -n "${NAME_SPACE}" -l 'app.kubernetes.io/component=backstage' -o jsonpath='{.items[0].spec.host}')"
+  local url="https://${RELEASE_NAME}-developer-hub-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
   check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "${url}"
-  # local rbac_url="https://${RELEASE_NAME_RBAC}-developer-hub-${NAME_SPACE_RBAC}.${K8S_CLUSTER_ROUTER_BASE}"
-  local rbac_url="https://$(oc get route -n "${NAME_SPACE_RBAC}" -l 'app.kubernetes.io/component=backstage' -o jsonpath='{.items[0].spec.host}')"
+  local rbac_url="https://${RELEASE_NAME_RBAC}-developer-hub-${NAME_SPACE_RBAC}.${K8S_CLUSTER_ROUTER_BASE}"
   check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "${rbac_url}"
 }
