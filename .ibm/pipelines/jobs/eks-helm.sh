686e25a4d44e4338a6d2daa0e3a006929e5511a2
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-12-03 11:31:28 +0100

    chore(ci): decouple namespace from Playwright project name

--- .ibm/pipelines/jobs/eks-helm.sh
+++ .ibm/pipelines/jobs/eks-helm.sh
@@ -33,7 +33,7 @@ handle_eks_helm() {
 
   initiate_eks_helm_deployment
   configure_eks_ingress_and_dns "${NAME_SPACE}" "${RELEASE_NAME}-developer-hub"
-  check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30
+  check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "showcase-k8s" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30
   cleanup_eks_dns_record "${EKS_INSTANCE_DOMAIN_NAME}"
   delete_namespace "${NAME_SPACE}"
 
@@ -44,7 +44,7 @@ handle_eks_helm() {
 
   initiate_rbac_eks_helm_deployment
   configure_eks_ingress_and_dns "${NAME_SPACE_RBAC}" "${RELEASE_NAME_RBAC}-developer-hub"
-  check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30
+  check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "showcase-rbac-k8s" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30
   cleanup_eks_dns_record "${EKS_INSTANCE_DOMAIN_NAME}"
   delete_namespace "${NAME_SPACE_RBAC}"
 }
--- .ibm/pipelines/jobs/gke-operator.sh
+++ .ibm/pipelines/jobs/gke-operator.sh
@@ -28,10 +28,10 @@ handle_gke_operator() {
   prepare_operator
 
   initiate_gke_operator_deployment "${NAME_SPACE}" "https://${K8S_CLUSTER_ROUTER_BASE}"
-  check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30
+  check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "showcase-k8s" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30
   delete_namespace "${NAME_SPACE}"
 
   initiate_rbac_gke_operator_deployment "${NAME_SPACE_RBAC}" "https://${K8S_CLUSTER_ROUTER_BASE}"
-  check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30
+  check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "showcase-rbac-k8s" "https://${K8S_CLUSTER_ROUTER_BASE}" 50 30
   delete_namespace "${NAME_SPACE_RBAC}"
 }
