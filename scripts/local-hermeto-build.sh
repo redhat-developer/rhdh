53017fa79bd7487d4b88b39f75a86048d7419dda
Frank Kong <frkong@redhat.com>
2025-12-05 15:28:45 -0500

    chore: refactor local hermeto install script
    Signed-off-by: Frank Kong <frkong@redhat.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- scripts/local-hermeto-build.sh
+++ scripts/local-hermeto-build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 #
 # Copyright (c) 2021-2025 Red Hat, Inc.
 # This program and the accompanying materials are made
@@ -7,18 +7,27 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 #
-# this script will extract the filesystem of a container to a folder
-# so you can browse its contents. Also works with scratch images
+# This script simulates the Konflux build process locally using Hermeto.
+# It can either build the dependency cache or build a container image.
+set -ex
+set -uo pipefail
 
-set -x
+#######################################
+# Constants
+#######################################
+readonly LOCAL_CACHE_BASEDIR='./hermeto-cache/'
+readonly HERMETO_IMAGE='quay.io/konflux-ci/hermeto:latest'
 
-LOCAL_CACHE_BASEDIR=./hermeto-cache/
-HERMETO_IMAGE=quay.io/konflux-ci/hermeto:latest
-
-
-usage ()
-{
-  echo "This script tries to somewhat simulate the Konflux build process.
+#######################################
+# Prints usage information and exits.
+# Globals:
+#   None
+# Arguments:
+#   None
+#######################################
+usage() {
+  cat << EOF
+This script tries to somewhat simulate the Konflux build process.
 
 Usage: $0 <type> <directory> [image]
 
@@ -28,95 +37,161 @@ Examples:
   $0 image . quay.io/example/image:tag
 
 Options:
-  <type>    The type of build. Options are:
-                - cache: Build the cache using Hermeto
-                - image: Build the image
+  <type>      The type of build. Options are:
+                  - cache: Build the cache using Hermeto
+                  - image: Build the image
   <directory> The directory of the component to build.
-  [image]   The name of the container image to build. Required for the 'image' type.
-  "
-  exit
+  [image]     The name of the container image to build. Required for 'image' type.
+EOF
+  exit 1
 }
 
-if [[ $# -lt 2 ]]; then usage; fi
+#######################################
+# Transforms a Containerfile to inject Hermeto/cachi2 configuration.
+# Globals:
+#   None
+# Arguments:
+#   containerfile: Path to the original Containerfile
+#   transformed_containerfile: Path to write the transformed Containerfile
+#######################################
+transform_containerfile() {
+  local containerfile="$1"
+  local transformed_containerfile="$2"
 
-TYPE=$1
-COMPONENT_DIR=$2
-IMAGE=$3
+  cp "${containerfile}" "${transformed_containerfile}"
 
-# Check if the type is valid
-if [[ "$TYPE" != "cache" && "$TYPE" != "image" ]]; then
-  echo "Invalid type: $TYPE"
-  usage
-fi
+  # Configure dnf to use the cachi2 repo
+  sed -i '/RUN *\(dnf\|microdnf\) install/i RUN rm -r /etc/yum.repos.d/* && cp /cachi2/output/deps/rpm/$(uname -m)/repos.d/hermeto.repo /etc/yum.repos.d/' \
+    "${transformed_containerfile}"
 
-# Check if image is provided for the 'image' type
-if [[ "$TYPE" == "image" && -z "$IMAGE" ]]; then
-  echo "Image name is required for the 'image' type."
-  usage
-fi
-
-function transformContainerfile() {
-    local containerfile="$1"
-    local transformed_containerfile="$2"
-
-    cp $containerfile $transformed_containerfile
-
-    # configure dnf to use the cachi2 repo
-    sed -i '/RUN *\(dnf\|microdnf\) install/i RUN rm -r /etc/yum.repos.d/* && cp /cachi2/output/deps/rpm/$(uname -m)/repos.d/hermeto.repo /etc/yum.repos.d/' $transformed_containerfile
-
-    # inject the cachi2 env variables to every RUN command
-    sed -i 's/^\s*RUN /RUN . \/cachi2\/cachi2.env \&\& /' $transformed_containerfile
+  # inject the cachi2 env variables to every RUN command
+  sed -i 's/^\s*RUN /RUN . \/cachi2\/cachi2.env \&\& /' $transformed_containerfile
 }
 
-COMPONENT_DIR=$(realpath "$COMPONENT_DIR")
-LOCAL_CACHE_DIR=$(realpath "$LOCAL_CACHE_BASEDIR")/$(basename "$COMPONENT_DIR")
-LOCAL_CACHE_OUTPUT_DIR=$LOCAL_CACHE_DIR/output
-echo "Component dir: $COMPONENT_DIR"
-echo "Local cache dir: $LOCAL_CACHE_DIR"
+#######################################
+# Builds the dependency cache using Hermeto.
+# Globals:
+#   HERMETO_IMAGE
+# Arguments:
+#   local_cache_dir: Path to the local cache directory
+#   local_cache_output_dir: Path to the cache output directory
+#######################################
+build_cache() {
+  local local_cache_dir="$1"
+  local local_cache_output_dir="$2"
 
-#############
-### CACHE ###
-#############
-if [[ "$TYPE" == "cache" ]]; then
-  # ensure the local cache dir exists
-  mkdir -p $LOCAL_CACHE_OUTPUT_DIR
+  # Ensure the local cache dir exists
+  mkdir -p "${local_cache_output_dir}"
 
-  # ensure the latest hermeto image
-  podman pull $HERMETO_IMAGE
-  # build cache
-  podman run --rm -ti -v "$PWD:/source:z" -v "$LOCAL_CACHE_DIR":/cachi2:z -w /source $HERMETO_IMAGE \
-      --log-level DEBUG \
-      fetch-deps --dev-package-managers  \
-      --source . \
-      --output /cachi2/output \
-      '[{"type": "rpm", "path": "."}, {"type": "yarn","path": "."}, {"type": "yarn","path": "./dynamic-plugins"}, {"type": "pip","path": "./python", "allow_binary": "false"}]'
+  # Ensure the latest hermeto image
+  podman pull "${HERMETO_IMAGE}"
 
-  podman run --rm -ti -v "$PWD:/source:z" -v "$LOCAL_CACHE_DIR":/cachi2:z -w /source $HERMETO_IMAGE \
+  # Build cache
+  podman run --rm -ti \
+    -v "${PWD}:/source:z" \
+    -v "${local_cache_dir}:/cachi2:z" \
+    -w /source \
+    "${HERMETO_IMAGE}" \
+    --log-level DEBUG \
+    fetch-deps --dev-package-managers \
+    --source . \
+    --output /cachi2/output \
+    '[{"type": "rpm", "path": "."}, {"type": "yarn","path": "."}, {"type": "yarn","path": "./dynamic-plugins"}, {"type": "pip","path": "./python", "allow_binary": "false"}]'
+
+  podman run --rm -ti \
+    -v "${PWD}:/source:z" \
+    -v "${local_cache_dir}:/cachi2:z" \
+    -w /source \
+    "${HERMETO_IMAGE}" \
     generate-env --format env --output /cachi2/cachi2.env /cachi2/output
 
-  podman run --rm -ti -v "$PWD:/source:z" -v "$LOCAL_CACHE_DIR":/cachi2:z -w /source $HERMETO_IMAGE \
+  podman run --rm -ti \
+    -v "${PWD}:/source:z" \
+    -v "${local_cache_dir}:/cachi2:z" \
+    -w /source \
+    "${HERMETO_IMAGE}" \
     inject-files /cachi2/output
-fi
+  return 0
+}
 
-#############
-### IMAGE ###
-#############
-if [[ "$TYPE" == "image" ]]; then
+#######################################
+# Builds a container image using the hermeto cache.
+# Globals:
+#   None
+# Arguments:
+#   component_dir: Path to the component directory
+#   local_cache_dir: Path to the local cache directory
+#   image: Name of the container image to build
+#######################################
+build_image() {
+  local component_dir="$1"
+  local local_cache_dir="$2"
+  local image="$3"
 
-  # ensure the local cache dir exists
-  if [[ ! -d "$LOCAL_CACHE_DIR" ]]; then
+  # Ensure the local cache dir exists
+  if [[ ! -d "${local_cache_dir}" ]]; then
     echo "Local cache dir does not exist. Please run the script with 'cache' first."
-    echo "example: $0 cache $2"
+    echo "example: $0 cache ${component_dir}"
     exit 1
   fi
 
-  # transform the containerfile to simulate Konflux build
-  transformContainerfile "$COMPONENT_DIR/docker/Containerfile" "$COMPONENT_DIR/docker/Containerfile.hermeto"
+  # Transform the containerfile to simulate Konflux build
+  transform_containerfile \
+    "${component_dir}/docker/Containerfile" \
+    "${component_dir}/docker/Containerfile.hermeto"
 
-  podman build -t "$IMAGE" \
-      --network none \
-      --no-cache \
-      -f "$COMPONENT_DIR"/docker/Containerfile.hermeto \
-      -v "$LOCAL_CACHE_DIR":/cachi2 \
-      "$COMPONENT_DIR"
-fi
+  podman build -t "${image}" \
+    --network none \
+    --no-cache \
+    -f "${component_dir}/docker/Containerfile.hermeto" \
+    -v "${local_cache_dir}:/cachi2" \
+    "${component_dir}"
+}
+
+#######################################
+# Main entry point for the script.
+# Globals:
+#   LOCAL_CACHE_BASEDIR
+# Arguments:
+#   Command line arguments
+#######################################
+main() {
+  if [[ $# -lt 2 ]]; then
+    usage
+  fi
+
+  local type="$1"
+  local component_dir="$2"
+  local image="${3:-}"
+
+  # Check if the type is valid
+  if [[ "${type}" != "cache" && "${type}" != "image" ]]; then
+    echo "Invalid type: ${type}"
+    usage
+  fi
+
+  # Check if image is provided for the 'image' type
+  if [[ "${type}" == "image" && -z "${image}" ]]; then
+    echo "Image name is required for the 'image' type."
+    usage
+  fi
+
+  # Resolve paths
+  local resolved_component_dir="$(realpath "${component_dir}")"
+  local local_cache_dir="$(realpath "${LOCAL_CACHE_BASEDIR}")/$(basename "${resolved_component_dir}")"
+  local local_cache_output_dir="${local_cache_dir}/output"
+
+  echo "Component dir: ${resolved_component_dir}"
+  echo "Local cache dir: ${local_cache_dir}"
+
+  case "${type}" in
+    cache)
+      build_cache "${local_cache_dir}" "${local_cache_output_dir}"
+      ;;
+    image)
+      build_image "${resolved_component_dir}" "${local_cache_dir}" "${image}"
+      ;;
+  esac
+}
+
+main "$@"
