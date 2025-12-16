8597199bc68e67be82e7245a347fce7a3bc73372
Gustavo Lira <guga.java@gmail.com>
2025-12-16 18:50:46 -0300

    fix(ci): correct deployment name in OpenShift Pipelines checks
    Updated the deployment name in the cluster_setup_ocp_helm() and cluster_setup_ocp_operator() functions to ensure accurate waiting for the OpenShift Pipelines operator. Additionally, refined the pod name retrieval logic in the k8s_wait::deployment function for improved reliability in identifying pods. This change enhances the overall accuracy of the deployment checks.

--- .ibm/pipelines/utils.sh
+++ .ibm/pipelines/utils.sh
@@ -541,7 +541,7 @@ cluster_setup_ocp_helm() {
 
   # Wait for OpenShift Pipelines to be ready before proceeding
   log::info "Waiting for OpenShift Pipelines to be ready..."
-  k8s_wait::deployment "openshift-operators" "openshift-pipelines-operator" 30 10 || return 1
+  k8s_wait::deployment "openshift-operators" "pipelines" 30 10 || return 1
   k8s_wait::endpoint "tekton-pipelines-webhook" "openshift-pipelines" 30 10 || return 1
 
   operator::install_postgres_ocp
@@ -559,7 +559,7 @@ cluster_setup_ocp_operator() {
 
   # Wait for OpenShift Pipelines to be ready before proceeding
   log::info "Waiting for OpenShift Pipelines to be ready..."
-  k8s_wait::deployment "openshift-operators" "openshift-pipelines-operator" 30 10 || return 1
+  k8s_wait::deployment "openshift-operators" "pipelines" 30 10 || return 1
   k8s_wait::endpoint "tekton-pipelines-webhook" "openshift-pipelines" 30 10 || return 1
 
   operator::install_postgres_ocp
