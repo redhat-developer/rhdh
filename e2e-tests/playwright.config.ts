6388b6ed100b10d47fb2a5747529c1702b062746
albarbaro <abarbaro@redhat.com>
2026-01-20 15:03:08 +0100

    add gitlab authentication scenario

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -196,10 +196,7 @@ export default defineConfig({
     },
     {
       name: PW_PROJECT.ANY_TEST,
-      //testMatch: "**/*.spec.ts",
-      testMatch: [
-        "**/playwright/e2e/auth-providers/gitlab.spec.ts",
-      ],
+      testMatch: "**/*.spec.ts",
     },
     {
       name: PW_PROJECT.SHOWCASE_UPGRADE,
