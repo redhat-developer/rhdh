a99008f886675fae957740852470e4232be10f45
Gustavo Lira <guga.java@gmail.com>
2026-01-21 08:13:15 -0300

    Testing

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -4,7 +4,7 @@ import { PW_PROJECT } from "./playwright/projects";
 process.env.JOB_NAME = process.env.JOB_NAME || "";
 process.env.IS_OPENSHIFT = process.env.IS_OPENSHIFT || "";
 
-const isPrE2eOcpHelmJob =
+const isPrOcpHelmJob =
   process.env.JOB_NAME.includes("pull") &&
   process.env.JOB_NAME.includes("e2e-ocp-helm");
 
@@ -84,7 +84,7 @@ export default defineConfig({
         "**/playwright/e2e/plugins/tekton/tekton.spec.ts",
         "**/playwright/e2e/dynamic-home-page-customization.spec.ts",
         "**/playwright/e2e/plugins/scorecard/scorecard.spec.ts",
-        ...(isPrE2eOcpHelmJob
+        ...(isPrOcpHelmJob
           ? ["**/playwright/e2e/plugins/orchestrator/**/*.spec.ts"]
           : []),
       ],
