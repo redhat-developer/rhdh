cf3ebf905ae947c0f0b8af0c78800d930edf0f62
Kashish Mittal <kmittal@redhat.com>
2026-01-25 21:01:37 -0500

    fix tsc errors
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>

--- e2e-tests/playwright/support/api/github-events.ts
+++ e2e-tests/playwright/support/api/github-events.ts
@@ -6,6 +6,28 @@ type CatalogAction = "added" | "modified" | "removed";
 type TeamAction = "created" | "deleted";
 type MembershipAction = "added" | "removed";
 
+interface GitHubUser {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  user_view_type: string;
+  site_admin: boolean;
+}
+
 interface Commit {
   id: string;
   tree_id: string;
@@ -129,7 +151,7 @@ interface TeamPayload {
 interface MembershipPayload {
   action: MembershipAction;
   scope: "team";
-  member: TeamPayload["sender"];
+  member: GitHubUser;
   sender: TeamPayload["sender"];
   team: Team;
   organization: TeamPayload["organization"];
