5eafc4da05f510fb944850d62b60273e319cca02
Frank Kong <frkong@redhat.com>
2026-02-02 13:40:18 -0500

    chore: update licensing
    Signed-off-by: Frank Kong <frkong@redhat.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- scripts/update-Dockerfile.sh
+++ scripts/update-Dockerfile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) Red Hat, Inc.
+# Copyright Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
