3300d13ff07ccae3056354f33fe41a738e273df8
Frank Kong <frkong@redhat.com>
2026-02-09 14:42:03 -0500

    chore: fix empty image reference bug when no platform is specified
    Signed-off-by: Frank Kong <frkong@redhat.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- scripts/local-hermeto-build.sh
+++ scripts/local-hermeto-build.sh
@@ -167,11 +167,11 @@ build_cache() {
   mkdir -p "${local_cache_output_dir}"
 
   # Ensure the latest hermeto image
-  podman pull "${platform_args}" "${HERMETO_IMAGE}"
+  podman pull ${platform_args} "${HERMETO_IMAGE}"
 
   # Build cache
   podman run --rm -ti \
-    "${platform_args}" \
+    ${platform_args} \
     -v "${PWD}:/source:z" \
     -v "${local_cache_dir}:/cachi2:z" \
     -w /source \
@@ -183,7 +183,7 @@ build_cache() {
     '[{"type": "rpm", "path": "."}, {"type": "yarn","path": "."}, {"type": "yarn","path": "./dynamic-plugins"}, {"type": "pip","path": "./python", "allow_binary": "false"}]'
 
   podman run --rm -ti \
-    "${platform_args}" \
+    ${platform_args} \
     -v "${PWD}:/source:z" \
     -v "${local_cache_dir}:/cachi2:z" \
     -w /source \
@@ -191,7 +191,7 @@ build_cache() {
     generate-env --format env --output /cachi2/cachi2.env /cachi2/output
 
   podman run --rm -ti \
-    "${platform_args}" \
+    ${platform_args} \
     -v "${PWD}:/source:z" \
     -v "${local_cache_dir}:/cachi2:z" \
     -w /source \
@@ -244,7 +244,7 @@ build_image() {
   trap 'rm -rf "${empty_dir}"' EXIT
 
   podman build -t "${image}" \
-    "${platform_args}" \
+    ${platform_args} \
     --network none \
     --no-cache \
     -f "${component_dir}/docker/Containerfile.hermeto" \
