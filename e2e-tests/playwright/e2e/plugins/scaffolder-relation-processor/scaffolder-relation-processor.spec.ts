78ef4ea156dc1b91321b88e5a344b00482dca692
Christoph Jerolimov <jerolimov+git@redhat.com>
2026-02-10 15:42:09 +0100

    fix(e2e-tests): prettier
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>

--- e2e-tests/playwright/e2e/plugins/scaffolder-relation-processor/scaffolder-relation-processor.spec.ts
+++ e2e-tests/playwright/e2e/plugins/scaffolder-relation-processor/scaffolder-relation-processor.spec.ts
@@ -85,7 +85,10 @@ test.describe.fixme("Test Scaffolder Relation Processor Plugin", () => {
 
     await uiHelper.clickButton("Create");
 
-    const linkLocator = page.locator("a").filter({ hasText: "Open in catalog" }).first();
+    const linkLocator = page
+      .locator("a")
+      .filter({ hasText: "Open in catalog" })
+      .first();
     // scaffolder might take a bit longer to create the entity, so we wait for the link to be visible before clicking
     await linkLocator.waitFor({ state: "visible", timeout: 60_000 });
     await linkLocator.click();
