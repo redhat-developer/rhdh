c2ed12a5e54e82b8bf14d37f0e3570b15284dfb1
Gustavo Lira <guga.java@gmail.com>
2026-02-18 09:57:56 -0300

    Testing

--- .ibm/pipelines/jobs/ocp-pull.sh
+++ .ibm/pipelines/jobs/ocp-pull.sh
@@ -17,82 +17,10 @@ handle_ocp_pull() {
   K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
   export K8S_CLUSTER_ROUTER_BASE
   cluster_setup_ocp_helm
-
-  cd "${DIR}" || return 1
-  # PR jobs: deploy with orchestrator disabled to avoid SonataFlow deployment timeouts.
-  # Orchestrator is fully tested in nightly jobs.
-  base_deployment_pr
-  rbac_deployment_pr
-
+  initiate_deployments
   deploy_test_backstage_customization_provider "${NAME_SPACE}"
   local url="https://${RELEASE_NAME}-developer-hub-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
   check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "${PW_PROJECT_SHOWCASE}" "${url}"
   local rbac_url="https://${RELEASE_NAME_RBAC}-developer-hub-${NAME_SPACE_RBAC}.${K8S_CLUSTER_ROUTER_BASE}"
   check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "${PW_PROJECT_SHOWCASE_RBAC}" "${rbac_url}"
-}
-
-# Base deployment with orchestrator disabled (for PR jobs)
-base_deployment_pr() {
-  configure_namespace ${NAME_SPACE}
-
-  deploy_redis_cache "${NAME_SPACE}"
-
-  cd "${DIR}" || return 1
-  local rhdh_base_url="https://${RELEASE_NAME}-developer-hub-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
-  apply_yaml_files "${DIR}" "${NAME_SPACE}" "${rhdh_base_url}"
-
-  # Merge base values with PR diff file to disable orchestrator
-  yq_merge_value_files "merge" "${DIR}/value_files/${HELM_CHART_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_PR_DIFF_VALUE_FILE_NAME}" "/tmp/merged-values_showcase_PR.yaml"
-  mkdir -p "${ARTIFACT_DIR}/${NAME_SPACE}"
-  cp -a "/tmp/merged-values_showcase_PR.yaml" "${ARTIFACT_DIR}/${NAME_SPACE}/" || true
-
-  log::info "Deploying image from repository: ${QUAY_REPO}, TAG_NAME: ${TAG_NAME}, in NAME_SPACE: ${NAME_SPACE}"
-
-  # shellcheck disable=SC2046
-  helm upgrade -i "${RELEASE_NAME}" -n "${NAME_SPACE}" \
-    "${HELM_CHART_URL}" --version "${CHART_VERSION}" \
-    -f "/tmp/merged-values_showcase_PR.yaml" \
-    --set global.clusterRouterBase="${K8S_CLUSTER_ROUTER_BASE}" \
-    $(get_image_helm_set_params)
-
-  log::warn "Skipping orchestrator workflows deployment for PR job"
-}
-
-# RBAC deployment with orchestrator disabled (for PR jobs)
-rbac_deployment_pr() {
-  configure_namespace "${NAME_SPACE_POSTGRES_DB}"
-  configure_namespace "${NAME_SPACE_RBAC}"
-  configure_external_postgres_db "${NAME_SPACE_RBAC}"
-
-  # Wait for PostgreSQL primary pod to be Running and Ready before deploying RHDH.
-  # configure_external_postgres_db only waits for secrets; the pod may still be starting.
-  # Without orchestrator the helm install finishes fast and RHDH tries to connect
-  # before PostgreSQL is accepting connections, causing a permanent startup failure.
-  # The 'role=master' label is only set by Patroni after the pod is fully running,
-  # so we use the 'data=postgres' label which is set at pod creation time.
-  log::info "Waiting for PostgreSQL primary pod to be ready in ${NAME_SPACE_POSTGRES_DB} (up to 10 min)..."
-  oc wait --for=condition=Ready pod \
-    -l postgres-operator.crunchydata.com/cluster=postgress-external-db,postgres-operator.crunchydata.com/data=postgres \
-    -n "${NAME_SPACE_POSTGRES_DB}" --timeout=600s
-  log::info "PostgreSQL primary pod is ready."
-
-  local rbac_rhdh_base_url="https://${RELEASE_NAME_RBAC}-developer-hub-${NAME_SPACE_RBAC}.${K8S_CLUSTER_ROUTER_BASE}"
-  apply_yaml_files "${DIR}" "${NAME_SPACE_RBAC}" "${rbac_rhdh_base_url}"
-
-  # Merge RBAC values with PR diff file to disable orchestrator
-  yq_merge_value_files "merge" "${DIR}/value_files/${HELM_CHART_RBAC_VALUE_FILE_NAME}" "${DIR}/value_files/${HELM_CHART_RBAC_PR_DIFF_VALUE_FILE_NAME}" "/tmp/merged-values_showcase-rbac_PR.yaml"
-  mkdir -p "${ARTIFACT_DIR}/${NAME_SPACE_RBAC}"
-  cp -a "/tmp/merged-values_showcase-rbac_PR.yaml" "${ARTIFACT_DIR}/${NAME_SPACE_RBAC}/" || true
-
-  log::info "Deploying image from repository: ${QUAY_REPO}, TAG_NAME: ${TAG_NAME}, in NAME_SPACE: ${RELEASE_NAME_RBAC}"
-
-  # shellcheck disable=SC2046
-  helm upgrade -i "${RELEASE_NAME_RBAC}" -n "${NAME_SPACE_RBAC}" \
-    "${HELM_CHART_URL}" --version "${CHART_VERSION}" \
-    -f "/tmp/merged-values_showcase-rbac_PR.yaml" \
-    --set global.clusterRouterBase="${K8S_CLUSTER_ROUTER_BASE}" \
-    $(get_image_helm_set_params)
-
-  log::warn "Skipping sonataflow database workaround for PR job"
-  log::warn "Skipping orchestrator workflows deployment for PR job"
 }
