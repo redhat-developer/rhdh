74564a6056e69aba4537ad0430faceb45005e52d
Nick Boldt <nboldt@redhat.com>
2026-06-01 13:25:11 -0300

    chore: breaking change - remove wrappers from RHDH entirely (RHDHPLAN-256, RHDHPLAN-934)
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- dynamic-plugins/wrappers/red-hat-developer-hub-backstage-plugin-extensions/src/alpha.ts
+++
@@ -1,4 +0,0 @@
-import { extensionsTranslationRef, extensionsTranslations} from "@red-hat-developer-hub/backstage-plugin-extensions/alpha";
-
-
-export { extensionsTranslationRef, extensionsTranslations, extensionsTranslationRef as marketplaceTranslationRef, extensionsTranslations as marketplaceTranslations };// NOSONAR
