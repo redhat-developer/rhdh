b1865ccaf5420f69e57d450340797aa8dbc184a2
Frank Kong <frkong@redhat.com>
2026-08-16 22:36:46 -0400

    chore: remove hardcoded sidebar logo in favor of global header plugin
    Signed-off-by: Frank Kong <frkong@redhat.com>
    
    rh-pre-commit.version: 2.4.0
    rh-pre-commit.check-secrets: ENABLED

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -2,5 +2,6 @@ import '@backstage/cli/asset-types';
 import ReactDOM from 'react-dom/client';
 import app from './App';
 import '@backstage/ui/css/styles.css';
+import 'material-icons/iconfont/outlined.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(app);
--- packages/app/src/modules/nav/Sidebar.tsx
+++ packages/app/src/modules/nav/Sidebar.tsx
@@ -9,23 +9,9 @@ import {
 import { NavContentBlueprint } from '@backstage/plugin-app-react';
 import { SidebarSearchModal } from '@backstage/plugin-search';
 import { UserSettingsSignInAvatar } from '@backstage/plugin-user-settings';
-import { useAppDrawer } from '@red-hat-developer-hub/backstage-plugin-app-react';
-import ChatIcon from '@mui/icons-material/Chat';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 
-import { SidebarLogo } from './SidebarLogo';
-
-const ChatDrawerItem = () => {
-  const { toggleDrawer } = useAppDrawer();
-  return (
-    <SidebarItem
-      icon={() => <ChatIcon />}
-      text="Chat"
-      onClick={() => toggleDrawer('lightspeed')}
-    />
-  );
-};
 
 export const SidebarContent = NavContentBlueprint.make({
   params: {
@@ -38,7 +24,6 @@ export const SidebarContent = NavContentBlueprint.make({
 
       return (
         <Sidebar>
-          <SidebarLogo />
           <SidebarGroup label="Search" icon={<SearchIcon />} to="/search">
             <SidebarSearchModal />
           </SidebarGroup>
@@ -53,8 +38,6 @@ export const SidebarContent = NavContentBlueprint.make({
             </SidebarScrollWrapper>
           </SidebarGroup>
           <SidebarSpace />
-          <SidebarDivider />
-          <ChatDrawerItem />
           <SidebarDivider />
           <SidebarGroup
             label="Settings"
